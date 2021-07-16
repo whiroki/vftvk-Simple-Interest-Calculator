8a3f87a99e6809f852da8d73fae7773c2f5184b7
whiroki <46113410+whiroki@users.noreply.github.com>
2021-07-17 00:35:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
+    
+    if( document.getElementById("principal").value < 1 ) {
+            alert( "Please provide your name!" );
+            document.getElementById("principal").focus() ;
+            return false;
+        
     document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
 
 }
@@ -15,3 +21,4 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 
 }
+
