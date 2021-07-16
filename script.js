1442e32b3143f213a20e4f0ca826cd5b4e3bb99a
whiroki <46113410+whiroki@users.noreply.github.com>
2021-07-17 01:21:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function compute()
 function updateRate()
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 
 }
 
