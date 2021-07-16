b88fda9a054574fe29a1683260f9da34d235ccd1
whiroki <46113410+whiroki@users.noreply.github.com>
2021-07-17 00:26:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-
-    // Get the value of the input field with id="numb"
-    let x = document.getElementById("principal").value;
-    // If x is Not a Number or less than one or greater than 10
-    let text;
-    if (isNaN(x) || x < 1 ) {
-        alert ("Input not valid");
-    } else {
     document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
-    }
-
 
 }
 
