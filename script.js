6af6b1a8691a60465bfa553a247f1df53634b25e
whiroki <wnagase@gmail.com>
2021-07-16 20:32:00 +0000

    fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+"<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="\<br\>\<br\>If you deposit "+principal+"<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>\<br\>"
 
 }
 
