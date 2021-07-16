ae0fec695575ed1a12318a28e39ec3c12be84b14
whiroki <46113410+whiroki@users.noreply.github.com>
2021-07-17 00:41:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function compute()
     
     if( document.getElementById("principal").value < 1 ) {
             alert( "Please provide your name!" );
+    } else {
+    document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
+    }
             document.getElementById("principal").focus() ;
             return false;
         
-    document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
+    ///document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
 
 }
 
