f18b7527bd53ddb0abb22661f45fdf48ae4dae15
whiroki <46113410+whiroki@users.noreply.github.com>
2021-07-17 00:47:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     
     if(principal < 1 ) {
-            alert( "invalid data!" );
+            alert( "Enter a positive number!" );
     } else {
     document.getElementById("result").innerHTML ="\<br\>\<br\>If you deposit <mark>"+principal+"</mark><br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>\<br\>";
     }
